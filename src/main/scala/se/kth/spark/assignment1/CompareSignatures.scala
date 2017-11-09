7fd57637ca61d16f617ea9271fb6f02952a51128
Avneesh Vyas <avneeshvyas@gmail.com>
2017-11-09 20:16:51 +0100

    corrects a bug in signature compare

--- src/main/scala/se/kth/spark/assignment1/CompareSignatures.scala
+++ src/main/scala/se/kth/spark/assignment1/CompareSignatures.scala
@@ -3,13 +3,7 @@ package se.kth.spark.assignment1
 import scala.collection.immutable.Set
 
 class CompareSignatures {
-  def compare(sig1:Array[BigInt], sig2:Array[BigInt]): Double = {
-    var count = 0
-    for(i <- 0 to sig1.size -1) {
-      if(sig1(i) == sig2(i)){
-        count +=1
-      }
-    }    
-    return (count/sig1.size).toDouble
+  def compare(sig1:Array[BigInt], sig2:Array[BigInt]): Double = { 
+    return sig1.toSet.intersect(sig2.toSet).size.toDouble / sig1.toSet.size
   }
 }
