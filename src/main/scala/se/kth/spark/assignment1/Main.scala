20c8d271d770fd5f252323c7632dfcc5ed9beb5d
Avneesh Vyas <avneeshvyas@gmail.com>
2017-11-09 22:43:50 +0100

    Adds some documents for testing

--- src/main/scala/se/kth/spark/assignment1/Main.scala
+++ src/main/scala/se/kth/spark/assignment1/Main.scala
@@ -16,19 +16,26 @@ object Main {
     val hashShingles = shingle.createHashedShingles("src/main/resources/documents", k)
 
     val list = hashShingles.collect()
-    list.foreach(println)
+    //list.foreach(println)
 
     //compute jaccard similarity among documents
     for (i <- 0 to list.size - 2) {
       for (j <- i + 1 to list.size - 1) {
         val sim = jaccardCompare.computeJaccardSimilarity(list(i)._2, list(j)._2)
-        println(s"JaccardSim(File${i},File${j}) = ${sim}")
+        val file1 = list(i)._1.substring(list(i)._1.lastIndexOf("/")+1)
+        val file2 = list(j)._1.substring(list(j)._1.lastIndexOf("/")+1)
+        println(s"JaccardSim(${file1},${file2}) = ${sim}")
       }
     }
+    
+    println(s"********************************************************")
+    println(s"********************************************************")
+    println(s"********************************************************")
+    println(s"********************************************************")
 
     val minHashed = hashShingles.mapValues { case (s) => minHashing.sig(s.toList) }
     
-    minHashed.map{case(k,v) => (k,v.toList)}.collect().foreach(println)
+    //minHashed.map{case(k,v) => (k,v.toList)}.collect().foreach(println)
     
 
     val minHashedlist = minHashed.collect()
@@ -38,7 +45,9 @@ object Main {
     for (i <- 0 to minHashedlist.size - 2) {
       for (j <- i + 1 to minHashedlist.size - 1) {
         val sim = sigCompare.compare(minHashedlist(i)._2, minHashedlist(j)._2)
-        println(s"MinHashSim(File${i},File${j}) = ${sim}")
+        val file1 = minHashedlist(i)._1.substring(minHashedlist(i)._1.lastIndexOf("/")+1)
+        val file2 = minHashedlist(j)._1.substring(minHashedlist(j)._1.lastIndexOf("/")+1)
+        println(s"MinHashSim(${file1},${file2}) = ${sim}")
       }
     }
 
