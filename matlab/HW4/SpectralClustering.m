1c59cf56cf00322fa3251ab9ce7d2e982c3a83dc
Agrumas <agrumuldis@gmail.com>
2017-12-03 19:38:35 +0100

    adds graph of Fiedler Vector

--- matlab/HW4/SpectralClustering.m
+++ matlab/HW4/SpectralClustering.m
@@ -1,7 +1,7 @@
 clc;
-E = csvread('example1.dat');
-%E = csvread('example2.dat');
-k = 4;
+%E = csvread('example1.dat');
+E = csvread('example2.dat');
+k = 10;
 
 col1 = E(:,1);
 col2 = E(:,2);
@@ -11,23 +11,30 @@ A = full(As);
 
 G = graph(A,'OmitSelfLoops');
 p = plot(G,'layout','force','Marker','.','MarkerSize',4.5);
+title('Graph');
 axis equal
 
 
 D = diag(sum(A,2));
 L = (D^(-0.5))*A*(D^(-0.5));
 
-[X,D] = eigs( L, k, 'LM' );
+[X,D] = eigs( L, k, 'LM');
 
 % normalizing
 Y = X./sqrt(sum(X.^2,2));
+
+fv = sort(X(:,1));
+figure,
+plot(fv,'+');
+title('Sorted Fiedler Vector');
 
 idx = kmeans(Y,k);
 
 color = {'k','r','b','g','y'};
 
+cc=hsv(12);
 for i=1:k
-    highlight(p,find(idx==i),'NodeColor',color{i})
+    highlight(p,find(idx==i),'NodeColor',cc(i,:))
 end
 
 % plot 
@@ -37,7 +44,7 @@ for i=1:size(A,1)
   c = idx(i,1);
   for j=1:size(A,2)  
     if A(i,j) == 1
-        plot(i,j,'color', color{c}, 'marker', '+');
+        plot(i,j,'color', cc(c,:), 'marker', '+');
     end  
   end  
 end
